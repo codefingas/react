01f5c4ddb33ab0d966d486961e1ea026d5b6a852
Ezekiel Saturday <ezekielsaturday@Doro-Macbook.local>
2020-01-06 01:00:31 +0100

    nested components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Ninjas from './Ninjas';
 
 function App() {
   return (
     <div className="App">
      <h1>My first react app !</h1>
      <p>Welcome</p>
+     <Ninjas />
     </div>
   );
 }
